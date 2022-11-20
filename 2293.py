18dd1fb9d372c632d12c6b9e7bcee748eb0f00cd
Chanwoong1 <jcw7468@gmail.com>
2022-11-20 15:38:42 +0900

    bonus clear

--- 2293.py
+++ 2293.py
@@ -2,7 +2,10 @@
 
 n, k = map(int, input().split())
 lst = [int(input()) for _ in range(n)]
-answer = 0
-for i in range(len(lst)) :
-    for j in range(i) :
-        if ()
+dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+for i in lst:
+    for j in range(1, k + 1):
+        if j - i >= 0:
+            dp[j] += dp[j - i]
+print(dp[k])
