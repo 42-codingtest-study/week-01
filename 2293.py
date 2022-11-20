353e7983d198cc841699246ee47b5861a1617c07
Chanwoong1 <jcw7468@gmail.com>
2022-11-20 15:39:09 +0900

    bonus clear

--- 2293.py
+++ 2293.py
@@ -3,9 +3,9 @@
 n, k = map(int, input().split())
 lst = [int(input()) for _ in range(n)]
 dp = [0 for _ in range(k + 1)]
-dp[0] = 1
-for i in lst:
+dp[0] = 1	# 초기값 설정
+for i in lst:	# 반복문
     for j in range(1, k + 1):
-        if j - i >= 0:
+        if j - i >= 0:	# 
             dp[j] += dp[j - i]
 print(dp[k])
