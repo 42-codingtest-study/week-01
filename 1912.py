a40d6f5806c4e7524337af27bc259f8e3fddec8b
noeyiz <lamoureclatant@gmail.com>
2022-11-21 16:24:04 +0900

    1912번: 연속합

--- 1912.py
+++ 1912.py
@@ -3,11 +3,11 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # 배열 생성 및 초기화
-dp = []
-dp.append(arr[0])
+dp = [0 for _ in range(n)]
+dp[0] = arr[0]
 
-# dp[i] + arr[i + 1]와 arr[i + 1] 중 더 큰 값을 저장하는 이유 : dp[i]가 음수일 경우
-for i in range(n - 1):
-    dp.append(max(dp[i] + arr[i + 1], arr[i + 1]))
+# dp[i - 1] + arr[i]와 arr[i] 중 더 큰 값을 저장하는 이유 : dp[i - 1]이 음수일 경우
+for i in range(1, n):
+    dp[i] = max(dp[i - 1] + arr[i], arr[i])
 
 print(max(dp))
